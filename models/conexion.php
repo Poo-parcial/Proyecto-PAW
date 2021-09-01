87187e6cf2f481e26a74ea2eb12619c4566f2931
Yeimy17 <73766199+Poo-parcial@users.noreply.github.com>
2021-09-01 09:54:28 -0600

    update

--- models/conexion.php
+++ models/conexion.php
@@ -6,8 +6,8 @@
         {
             if(strtoupper(substr(PHP_OS,0,3)) == 'WIN' )
             {
-                $user = 'poo';
-                $pass = 'poo';
+                $user = 'admin';
+                $pass = '654321';
                 $host = 'localhost';
                 $bd = 'paw';
             }
