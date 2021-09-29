4ba027bdc7cab3c85a8a1b082a18884fe1b18ee1
Yeimy17 <73766199+Poo-parcial@users.noreply.github.com>
2021-09-29 10:50:29 -0600

    update

--- views/token.php
+++ views/token.php
@@ -1,2 +1,25 @@
 <?php
-?>
+ include  "../../../models/conexion.php";
+ include  "../../../controllers/procesos.php";
+ include " ../../../models/procesos.php ";
+ 
+ $user =_POST ["user"];
+ $email =_POST ["emal"];
+ 
+  $query1 = "SELECT COUNT (usuario) AS tuser from usuarios WHERE usuario = "$user"  AND email = "$user" ";
+ 
+ $query1 = "SELECT COUNT (email) AS temail from usuarios WHERE usuario = "$user"  AND email = "$EMAIL" ";
+ 
+  $buscauser = CRUD($query1, "s");
+  $buscauser = CRUD($query2, "s");
+  
+  foreach ($buscaemail AS $result)
+  {
+      echo $contuser = $result ["tuser"];
+  }
+  
+  foreach
+  {
+      
+  }
+ ?>
